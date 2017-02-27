9d51d1e32d76feac8728216e6ef3daa6fbf87583
chenyu <chenyu6543@qq.com>
2017-02-27 20:21:24 +0800

    添加工具类

---
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -0,0 +1,47 @@
+package cn.ucai.live.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
+
+import cn.ucai.live.I;
+import cn.ucai.live.R;
+import cn.ucai.live.ui.activity.ChatActivity;
+import cn.ucai.live.ui.activity.LoginActivity;
+import cn.ucai.live.ui.activity.MainActivity;
+import cn.ucai.live.ui.activity.RegisterActivity;
+
+
+/**
+ * 页面的跳转
+ */
+public class MFGT {
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity context, Class<?> cls) {
+        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+        context.startActivity(new Intent(context, cls));
+    }
+
+    public static void startActivity(Activity context, Intent intent) {
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void gotoChat(Activity activity, String userName) {
+        startActivity(activity,new Intent(activity, ChatActivity.class)
+        .putExtra("userId",userName));
+    }
+
+    public static void gotoMain(Activity activity){
+        startActivity(activity, new Intent(activity,MainActivity.class)
+        .putExtra(I.BACK_MAIN_FROM_CHAT,true));
+    }
+
+}
