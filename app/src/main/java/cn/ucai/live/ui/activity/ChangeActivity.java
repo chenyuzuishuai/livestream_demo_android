54d1eefe5f2b64074c2fa2c2c83e5fecfb1715a8
chenyu <chenyu6543@qq.com>
2017-03-06 12:28:21 +0800

    显示账户余额界面

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -2,16 +2,30 @@ package cn.ucai.live.ui.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 /**
  * Created by yu chen on 2017/3/6.
  */
-public class ChangeActivity extends BaseActivity{
+public class ChangeActivity extends BaseActivity {
+    @BindView(R.id.tv_change_balance)
+    TextView mTvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout mTargetLayout;
+    View loadView;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_change);
+        ButterKnife.bind(this);
+        loadView = LayoutInflater.from(ChangeActivity.this).inflate(R.layout.rp_loading,null);
+        mTargetLayout.addView(loadView);
     }
 }
